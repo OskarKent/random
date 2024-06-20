bd0de048a7148af00fecb339e6f6acd957a0c058
anywhen.com <robkent@users.noreply.github.com>
2024-06-20 16:53:26 -0400

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,9 @@
+var num1 = prompt("enter a number to be multiplied");
+var num2 = prompt("enter another number to be multiplied");
+var sum = Number(num1)*Number(num2);
+
+if (sum>0) {
+	alert("the answer is "+sum)
+}else{
+	alert("not a number")
+}
