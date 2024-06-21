192bf33aca90591dbc2f31e0a64c8ae8035488ba
anywhen.com <robkent@users.noreply.github.com>
2024-06-21 13:10:10 -0400

    wordstrech.js

--- wordstrech.js
+++ wordstrech.js
@@ -1,3 +1,3 @@
-alert("try highlighting the moving words so that the highlighting moves to")
+alert("try highlighting the moving words so that the highlighting moves too")
 alert("this webpage may not work properly. the words are supposed to move")
 
