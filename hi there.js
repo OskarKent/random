871dcdcc06db16ccf180e3e38e75d4c2b2820415
anywhen.com <robkent@users.noreply.github.com>
2024-06-20 20:57:39 -0400

    Add files via upload

---
+++ hi there.js
@@ -0,0 +1,30 @@
+alert("click hi there and then hold enter for an auto clicker")
+
+var hiThere=0;
+var button=document.getElementById("banana");
+var ul = document.querySelector("ul");
+
+function maketext() {
+	hiThere++;
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode("hi there!"));
+	ul.appendChild(li);
+	if (hiThere===101) {
+	li.appendChild(document.createTextNode("\n you made a hi there more than 100 times"));
+	ul.appendChild(li);
+}
+	if (hiThere===1001) {
+	li.appendChild(document.createTextNode("\n you made a hi there more than 1000 times"));
+	ul.appendChild(li);
+}
+	if (hiThere===10001) {
+	li.appendChild(document.createTextNode("\n you made a hi there more than 10000 times"));
+	ul.appendChild(li);
+}
+	if (hiThere===100001) {
+	li.appendChild(document.createTextNode("\n you made a hi there more than 100000 times"));
+	ul.appendChild(li);
+}
+}
+
+banana.addEventListener("click",maketext)
