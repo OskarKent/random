9e6d9b96dfa5ce3ba6ad290036d5d572957f53b3
anywhen.com <robkent@users.noreply.github.com>
2024-06-26 09:30:54 -0400

    game.js

--- game.js
+++ game.js
@@ -1,15 +1,13 @@
-const input = document.getElementById("input");
-const enter = document.getElementById("enter");
-let num = Math.floor(Math.random() * 100)
-
-const game = () =>{
-	if (Number(input.value) === Number(num)) {
-		alert("you win!");
-		input.value = "";
-	}else{
-		alert("wrong number");
-		input.value = "";
-	}
-}
-
-enter.addEventListener("click",game)
+var input = document.getElementById("input");
+var enter = document.getElementById("enter");
+var num = Math.floor(Math.random() * 100);
+var game = function game() {
+  if (Number(input.value) === Number(num)) {
+    alert("you win!");
+    input.value = "";
+  } else {
+    alert("wrong number");
+    input.value = "";
+  }
+};
+enter.addEventListener("click", game);
