4170574f04c56d5a03c101646f440f096bb241b6
Oskar <robkent@users.noreply.github.com>
2024-07-09 13:51:36 -0400

    grow.js

--- grow.js
+++ grow.js
@@ -1,12 +1,10 @@
-const img = document.getElementById("img");
-
-const makeBig = () =>{
-const h1 = document.getElementById("h1");
-let vwidth = img.clientWidth;
-let vheight = img.clientHeight;
-img.style.width = vwidth + 5 +'px';
-img.style.height = vheight + 5 +'px';
-h1.textContent = `your pizza is ${vheight} pixels heigh and ${vwidth} pixels wide`
-}
-
-img.addEventListener("click",makeBig)
+var img = document.getElementById("img");
+var makeBig = function makeBig() {
+  var h1 = document.getElementById("h1");
+  var vwidth = img.clientWidth;
+  var vheight = img.clientHeight;
+  img.style.width = vwidth + 5 + 'px';
+  img.style.height = vheight + 5 + 'px';
+  h1.textContent = "your pizza is ".concat(vheight, " pixels heigh and ").concat(vwidth, " pixels wide");
+};
+img.addEventListener("click", makeBig);
