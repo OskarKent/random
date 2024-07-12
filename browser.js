61ee11cb1453950d4aeecda4fc310d0a2cc94a57
Oskar <robkent@users.noreply.github.com>
2024-07-12 10:11:11 -0400

    Add files via upload

--- browser.js
+++ browser.js
@@ -1,13 +1,16 @@
-const button = document.getElementById("button");
-const input = document.getElementById("input");
-const a = document.getElementById("a");
-const h1 = document.getElementById("h1");
+var button = document.getElementById("button");
+var input = document.getElementById("input");
+var a = document.getElementById("a");
+var h1 = document.getElementById("h1");
 //document.addEventListener('DOMContentLoaded', () => {
 //document.getElementById('myLink').href = 'https://newexample.com';
-const go = () =>{
-	let num = input.value.length;
-	h1.textContent = "go to website"
-	a.href = input.value;
-}
-
-button.addEventListener("click",go)
+var go = function go() {
+  var num = input.value.length;
+  h1.textContent = "go to ".concat(input.value);
+  a.href = input.value;
+};
+var clear = function clear() {
+  h1.textContent = "";
+};
+button.addEventListener("click", go);
+a.addEventListener("click", clear);
